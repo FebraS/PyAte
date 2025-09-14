33f9308e45e247f573ea3d61961ee4f29b7e370c
Febra <0xfebra@gmail.com>
2025-09-14 10:17:17 +0800

    fix: Change the banner font from 'dos_rebel' to 'ansi_shadow'

--- utils/terminal.py
+++ utils/terminal.py
@@ -14,7 +14,7 @@ def banner():
     # Fix colorama issue on Windows
     init()
 
-    print(f"\n{Fore.LIGHTYELLOW_EX}{pyfiglet.figlet_format('PyAte', font='dos_rebel')}{Style.RESET_ALL}")
+    print(f"\n{Fore.LIGHTYELLOW_EX}{pyfiglet.figlet_format('PyAte', font='ansi_shadow')}{Style.RESET_ALL}")
     print(f"{Fore.LIGHTBLUE_EX}Python Authenticator Token Extractor{Style.RESET_ALL}")
     print(f"https://github.com/FebraS/PyAte")
     print("\n")
