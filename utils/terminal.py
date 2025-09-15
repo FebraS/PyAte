183d4c293ae918464767eb1621ab856ffb56399f
Febra <0xfebra@gmail.com>
2025-09-15 16:55:29 +0800

    fix: Update banner link color to light cyan
    This commit updates the color of the GitHub repository link in the banner from light blue to light cyan for better visual consistency.

--- utils/terminal.py
+++ utils/terminal.py
@@ -15,5 +15,5 @@ def banner():
 
     print(f"\n{Fore.LIGHTYELLOW_EX}{pyfiglet.figlet_format('PyAte', font='ansi_shadow')}{Style.RESET_ALL}")
     print(f"{Fore.LIGHTBLUE_EX}Python Authenticator Token Extractor{Style.RESET_ALL}")
-    print(f"https://github.com/FebraS/PyAte")
+    print(f"{Fore.LIGHTCYAN_EX}https://github.com/FebraS/PyAte{Style.RESET_ALL}")
     print("\n")
